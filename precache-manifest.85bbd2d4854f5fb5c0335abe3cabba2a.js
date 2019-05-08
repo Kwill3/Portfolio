ee02a6443dc15f33b14284c1482b2dc565b488a8
William Lee <karlweiyang@gmail,com>
2019-05-08 10:45:23 +0100

    Updates

---
+++ precache-manifest.85bbd2d4854f5fb5c0335abe3cabba2a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67597e602be77c8d83d4f3bfc7a0c1a1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "587519dd78332a803194",
+    "url": "/portfolio/static/css/main.81da217e.chunk.css"
+  },
+  {
+    "revision": "692b4a7096f4710b6407",
+    "url": "/portfolio/static/js/2.9656223f.chunk.js"
+  },
+  {
+    "revision": "587519dd78332a803194",
+    "url": "/portfolio/static/js/main.20a15bb7.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "760b61050e87a6405018c5b13edd9eb8",
+    "url": "/portfolio/static/media/bokeh.760b6105.jpg"
+  },
+  {
+    "revision": "75e4d6d5a0f6b8f4e4030f9b00511ca7",
+    "url": "/portfolio/static/media/css.75e4d6d5.png"
+  },
+  {
+    "revision": "3cacc98a71fda4115686d17b9834250d",
+    "url": "/portfolio/static/media/es6.3cacc98a.jpeg"
+  },
+  {
+    "revision": "9c8d8d4170db7ea7cadcaf21e7a68495",
+    "url": "/portfolio/static/media/gifme.9c8d8d41.PNG"
+  },
+  {
+    "revision": "b6ec19ceacb23390aa66d2f101d0ddf6",
+    "url": "/portfolio/static/media/github.b6ec19ce.png"
+  },
+  {
+    "revision": "2c1a7560c88ea83e6b2593cd07af8ad8",
+    "url": "/portfolio/static/media/gmail.2c1a7560.png"
+  },
+  {
+    "revision": "31039a53ab57f00b4eff33fb7bcfa2e8",
+    "url": "/portfolio/static/media/html5.31039a53.png"
+  },
+  {
+    "revision": "6a4c201cef84c174d0d93923f26b168c",
+    "url": "/portfolio/static/media/javascript.6a4c201c.png"
+  },
+  {
+    "revision": "07aa93b4c14dc6fc70ad781128b71e59",
+    "url": "/portfolio/static/media/js-calculator.07aa93b4.PNG"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/portfolio/static/media/react.f297dfcd.png"
+  },
+  {
+    "revision": "c187d0bcad54409a3430751181f8485b",
+    "url": "/portfolio/static/media/space-daily.c187d0bc.png"
+  },
+  {
+    "revision": "8a4eb203379140ed2210708690b84ba2",
+    "url": "/portfolio/static/media/tix.8a4eb203.PNG"
+  }
+]);
